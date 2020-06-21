39d7a647e72d4fbd48e591f20c20182e715c9b5e
fxf432@163.com <fxf432@163.com>
2020-06-21 15:20:11 +0800

    git init

---
+++ test/java/com/design/partterns/PartternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.design.partterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartternsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
